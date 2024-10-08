d31a2895aa8f9e52df60337974843b73de258365
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-08 16:46:08 +0200

    add turn off lamp function

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ const lampEl = document.getElementById('lamp');
 const buttonEl = document.getElementById('on_off');
 //dichiaro la funzione richiamando l'evento al click
 buttonEl.addEventListener('click', () => {
-//richiamo l'elemento da modificare ed associo la proprietà che mi permette di aggiungere un codice html
-lampEl.innerHTML = 
-// inserisco il link all'immagine alternativa
-'<img src="./img/yellow_lamp.png">'
-//cambio testo del pulsante una volta "accesa" la lampadina
-buttonEl.innerText = 'Spegni'
+    //richiamo l'elemento da modificare ed associo la proprietà che mi permette di aggiungere un codice html
+    lampEl.innerHTML =
+        // inserisco il link all'immagine alternativa
+        '<img src="./img/yellow_lamp.png">'
+    //cambio testo del pulsante una volta "accesa" la lampadina
+    buttonEl.innerText = 'Spegni'
+//ripeto la funzione per invertire il procedimento
+    buttonEl.addEventListener('click', () => {
 
+        lampEl.innerHTML = '<img src="./img/white_lamp.png">'
+        buttonEl.innerText = 'Accendi'
+
+    });
 });
