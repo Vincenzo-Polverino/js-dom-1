464d874bcbf560d6fe2c5e60501d8ac3c98c7434
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-08 16:57:21 +0200

    clean code

--- script.js
+++ script.js
@@ -4,27 +4,18 @@ console.log('ravanello');
 
 
 //inserisco le variabili
+
 const lampEl = document.getElementById('lamp');
 const buttonEl = document.getElementById('on_off');
+
 //dichiaro la funzione richiamando l'evento al click
 buttonEl.addEventListener('click', () => {
-    //richiamo l'elemento da modificare ed associo la proprietà che mi permette di aggiungere un codice html
-    lampEl.innerHTML =
-        // inserisco il link all'immagine alternativa
-        '<img src="./img/yellow_lamp.png">'
-    //cambio testo del pulsante una volta "accesa" la lampadina
-    buttonEl.innerText = 'Spegni'
-//ripeto la funzione per invertire il procedimento
-    buttonEl.addEventListener('click', () => {
 
-       if(buttonEl.innerText === 'Accendi') {
+    if (buttonEl.innerText === 'Accendi') {
         lampEl.innerHTML = '<img src="./img/yellow_lamp.png">'
         buttonEl.innerText = 'Spegni'
-    } else{
-
+    } else {
         lampEl.innerHTML = '<img src="./img/white_lamp.png">'
         buttonEl.innerText = 'Accendi'
     }
-
-    });
 });
