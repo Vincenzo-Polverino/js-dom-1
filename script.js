7d2d00a809a66d55294b1b13bae0e9e0ef2ed0a2
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-08 16:34:08 +0200

    complete function

--- script.js
+++ script.js
@@ -8,5 +8,9 @@ const lampEl = document.getElementById('lamp');
 const buttonEl = document.getElementById('on_off');
 //dichiaro la funzione richiamando l'evento al click
 buttonEl.addEventListener('click', () => {
-   
-  });
+//richiamo l'elemento da modificare ed associo la proprietà che mi permette di aggiungere un codice html
+lampEl.innerHTML = 
+// inserisco il link all'immagine alternativa
+'<img src="./img/yellow_lamp.png">'
+
+});
