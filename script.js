a440f98485f8d4fefb389520633cd683b778bf0a
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-08 16:29:18 +0200

    declare function

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ console.log('ravanello');
 //inserisco le variabili
 const lampEl = document.getElementById('lamp');
 const buttonEl = document.getElementById('on_off');
+//dichiaro la funzione richiamando l'evento al click
+buttonEl.addEventListener('click', () => {
+   
+  });
